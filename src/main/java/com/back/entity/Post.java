8eac2cacd89760c02a0690da0ac5f9257be502b7
Seung-9 <ssg9505fj22@naver.com>
2025-12-23 12:35:50 +0900

    0003

--- src/main/java/com/back/entity/Post.java
+++ src/main/java/com/back/entity/Post.java
@@ -1,11 +1,16 @@
 package com.back.entity;
 
+import static jakarta.persistence.CascadeType.PERSIST;
+import static jakarta.persistence.CascadeType.REMOVE;
 import static jakarta.persistence.FetchType.LAZY;
 
 import com.back.jpa.entity.BaseAndTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -14,6 +19,9 @@ public class Post extends BaseAndTime {
 
     @ManyToOne(fetch = LAZY)
     private Member author;
+
+    @OneToMany(mappedBy = "post", cascade = {PERSIST, REMOVE}, orphanRemoval = true)
+    private List<PostComment> comments = new ArrayList<>();
 
     private String title;
 
@@ -24,5 +32,17 @@ public class Post extends BaseAndTime {
         this.author = author;
         this.title = title;
         this.content = content;
+    }
+
+    public PostComment addComment(Member author, String content) {
+        PostComment postComment = new PostComment(this, author, content);
+
+        comments.add(postComment);
+
+        return postComment;
+    }
+
+    public boolean hasComments() {
+        return !comments.isEmpty();
     }
 }

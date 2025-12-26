f74bfac132c9e5b0a07289879bbb606d60709da7
Seung-9 <ssg9505fj22@naver.com>
2025-12-26 14:46:12 +0900

    0024 - 어노테이션 수정

--- src/main/java/com/back/global/jpa/entity/BaseManualIdAndTime.java
+++ src/main/java/com/back/global/jpa/entity/BaseManualIdAndTime.java
@@ -1,12 +1,12 @@
 package com.back.global.jpa.entity;
 
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 

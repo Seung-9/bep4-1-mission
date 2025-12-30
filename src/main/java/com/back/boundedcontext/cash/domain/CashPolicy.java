1cba5ed8a42b151a27ecdacd1fe605fa7391d017
Seung-9 <ssg9505fj22@naver.com>
2025-12-30 09:32:17 +0900

    0037

--- src/main/java/com/back/boundedcontext/cash/domain/CashPolicy.java
+++ src/main/java/com/back/boundedcontext/cash/domain/CashPolicy.java
@@ -1,5 +1,14 @@
 package com.back.boundedcontext.cash.domain;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class CashPolicy {
-    public static final int HOLDING_MEMBER_ID = 2;
+    public static int HOLDING_MEMBER_ID;
+
+    @Value("${spring.application.name}")
+    public void setHoldingMemberId(int id) {
+        HOLDING_MEMBER_ID = id;
+    }
 }
